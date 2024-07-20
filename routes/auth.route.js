3ffea95ea32ca208d189c1db3014ed026f26fd0c
gaikwadshrutii <gaikwadshrutii01@gmail.com>
2024-07-20 14:21:28 +0530

    admin auth complete

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -4,4 +4,5 @@ router
     .post("/register-admin", authController.registerAdmin)
     .post("/login-admin", authController.loginAdmin)
     .post("/verify-admin-otp", authController.verifyOTP)
+    .post("/logout-admin", authController.logoutAdmin)
 module.exports = router
