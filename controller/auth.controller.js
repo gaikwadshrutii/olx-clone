97b3e41343eac9bedef47969cd741e80ef68815d
gaikwadshrutii <gaikwadshrutii01@gmail.com>
2024-07-26 13:04:56 +0530

    user login resul

--- controller/auth.controller.js
+++ controller/auth.controller.js
@@ -174,7 +174,17 @@ exports.loginUser = asyncHandler(async (req, res) => {
         secure: process.env.NODE_ENV === "production",
         maxAge: 1000 * 60 * 60 * 24 * 180
     })
-    res.json({ message: "User Register Success" })
+    res.json({
+        message: "User Register Success", result: {
+            _id: result._id,
+            name: result.name,
+            email: result.email,
+            mobile: result.mobile,
+            avatar: result.avatar,
+            verified: result.verified,
+
+        }
+    })
 
 })
 exports.logoutUser = asyncHandler(async (req, res) => {
