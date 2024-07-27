8b9815b5aa812f5a2fd58030b64aba18987b137c
gaikwadshrutii <gaikwadshrutii01@gmail.com>
2024-07-27 12:28:06 +0530

    verify email

--- routes/user.route.js
+++ routes/user.route.js
@@ -3,5 +3,5 @@ const { userProteced } = require("../middlewere/protected")
 const userController = require("./../controller/user.controller")
 router
     .post("verify-user-email", userProteced, userController.verifyUserEmail)
-    
+
 module.exports = router
