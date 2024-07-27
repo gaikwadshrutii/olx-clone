3afe5ef5ea39991242e9291b92f373aef782d358
gaikwadshrutii <gaikwadshrutii01@gmail.com>
2024-07-27 13:12:38 +0530

    user email and mobile verify fix

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -3,6 +3,10 @@ const User = require("../model/User")
 const sendEmail = require("../utils/email")
 exports.verifyUserEmail = asyncHandler(async (req, res) => {
     const result = await User.findById(req.loggedInUser)
+    if (!result) {
+        return res.status(401).json({ message: "You are not login ,please login Again" })
+    }
+    console.log(req.loggedInUser)
     const otp = Math.floor(10000 + Math.random() * 900000)
     await User.findByIdAndUpdate(req.loggedInUser, { emailCode: otp })
     const isSend = await sendEmail({
@@ -11,4 +15,28 @@ exports.verifyUserEmail = asyncHandler(async (req, res) => {
         message: `<p> your otp is ${otp}</p>`
     })
     res.json({ message: "User Email Verify Success" })
+})
+exports.verifyEmailOpt = asyncHandler(async (req, res) => {
+    const { otp } = req.body
+    const result = await User.findById(req.loggedInUser)
+    if (!result) {
+        return res.status(401).json({ message: "You are not login ,please login Again" })
+    }
+    if (otp !== result.emailCode) {
+        return res.status(400).json({ message: "Invalid OTP" })
+    }
+    await User.findByIdAndUpdate(req.loggedInUser, { emailverified: true })
+    req.json({ message: "Email verify Success" })
+})
+exports.verifyMobileOTP = asyncHandler(async (req, res) => {
+    const { otp } = req.body
+    const result = await User.findById(req.loggedInUser)
+    if (!result) {
+        return res.status(401).json({ message: "You are not login ,please login Again" })
+    }
+    if (otp !== result.mobileCode) {
+        return res.status(400).json({ message: "Invalid OTP" })
+    }
+    await User.findByIdAndUpdate(req.loggedInUser, { mobileverified: true })
+    req.json({ message: "Mobile verify Success" })
 })
--- routes/user.route.js
+++ routes/user.route.js
@@ -3,5 +3,7 @@ const { userProteced } = require("../middlewere/protected")
 const userController = require("./../controller/user.controller")
 router
     .post("verify-user-email", userProteced, userController.verifyUserEmail)
+    .post("verify-user-email-otp", userProteced, userController.verifyEmailOpt)
+    .post("verify-user-mobile-otp", userProteced, userController.verifyMobileOTP)
 
 module.exports = router
